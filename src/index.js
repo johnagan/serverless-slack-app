73396b8e6671efc024f8c966b8540033eb947173
Steve Paulo <steve.paulo@invitae.com>
2019-09-25 13:31:53 -0700

    added slash commands to send and get messages to/from SQS

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@
 // Include the serverless-slack bot framework
 const slack = require('serverless-slack');
 
+// Set up AWS interface
+const AWS = require('aws-sdk');
+AWS.config.update({region: 'us-east-1'});
+const sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+const queueUrl = "https://sqs.us-east-2.amazonaws.com/578417282904/CROP.fifo"
+
 
 // The function that AWS Lambda will call
 exports.handler = slack.handler.bind(slack);
@@ -25,16 +31,16 @@ slack.on('/greet', (msg, bot) => {
   };
 
   // ephemeral reply
-  bot.replyPrivate(message); 
+  bot.replyPrivate(message);
 });
 
 
 // Interactive Message handler
 slack.on('greetings_click', (msg, bot) => {
-  let message = { 
+  let message = {
     // selected button value
-    text: msg.actions[0].value 
-  };  
+    text: msg.actions[0].value
+  };
 
   // public reply
   bot.reply(message);
@@ -43,7 +49,60 @@ slack.on('greetings_click', (msg, bot) => {
 
 // Reaction Added event handler
 slack.on('reaction_added', (msg, bot) => {
-  bot.reply({ 
-    text: ':wave:' 
+  bot.reply({
+    text: ':wave:'
+  });
+});
+
+// Test sending a message to the queue
+slack.on('/sqsSend', (_msg, bot) => {
+  const params = {
+    MessageBody: JSON.stringify({
+      text: "This message was sent to SQS at the date and time attached.",
+      date: (new Date()).toISOString()
+    }),
+    QueueUrl: queueUrl
+  };
+
+  sqs.sendMessage(params, (err, data) => {
+    if (err) {
+      bot.reply(`There was an error sending a message to SQS: ${err}`);
+    } else {
+      bot.reply(`Successfully added a message to the SQS queue! Message ID: ${data.MessageId}`);
+    }
+  });
+});
+
+// Test retrieving, then deleting, a message from the queue
+slack.on('/sqsGet', (_msg, bot) => {
+  const params = {
+    QueueUrl: queueUrl,
+    MaxNumberOfMessages: 1,
+    VisibilityTimeout: 0,
+    WaitTimeSeconds: 0
+  };
+
+  sqs.receiveMessage(params, (err, data) => {
+    if (err) {
+      bot.reply(`There was an error retrieving a message from SQS: ${err}`);
+    } else {
+      if (!data.Message) {
+        bot.reply("Nothing to process.");
+        return;
+      }
+      const retrievedMessage = JSON.parse(data.Messages[0].Body);
+      bot.reply(`Message retrieved: ${retrievedMessage}`);
+      const deleteParams = {
+        QueueUrl: queueUrl,
+        ReceiptHandle: data.Messages[0].ReceiptHandle
+      };
+      sqs.deleteMessage(deleteParams, (err, _data) => {
+        if (err) {
+          bot.reply(`There was an error deleting a message from SQS: ${err}`);
+        } else {
+          bot.reply("Successfully deleted the last message from SQS.");
+        }
+      });
+    }
   });
 });
